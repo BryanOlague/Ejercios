0afe317e9f1d9ad11eb6ffc355eb8f99874f7a7c
Bryan Olague <bryanolague5@gmail.com>
2026-03-23 18:20:55 -0600

    Agregando metodos estaticos y de instancia

--- objetosAvanzados.js
+++ objetosAvanzados.js
@@ -38,3 +38,33 @@ console.log(programmer.language);
 programmer.greet();
 programmer.sayAge();
 
+
+// -- Metodos estaticos y de instancia 
+
+function Person (name,age){
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.greet = () => {
+    console.log(`Hola, soy: ${this.name}`);
+}
+
+let new_person = new Person("Olague",27);
+new_person.greet();
+
+// Metodos avanzados 
+// assign ---------------- sirve para unir las propiedades de objetos 
+
+let personCore = {name: "Bryan"}; 
+let personDetails = {age: 24, alias:"Olague"};
+
+let fullPerson = Object.assign(personCore, personDetails);   // para unir utilizamos object.assig (Base, lo que se desea agregar)
+console.log(fullPerson); // todo junto lo guardamos en un objeto
+
+// keys, values , entries 
+
+console.log(Object.keys(fullPerson));    // obtenemos las claves del objeto 
+console.log(Object.values(fullPerson));     // obtenemos solamente los valores del objeto
+console.log(Object.entries(fullPerson));        //obtenemos array con las claves y los valores 
+
